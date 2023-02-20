4dc9d3387787804d667f4ce424de36ebe539fe53
albertobaggio96 <albertobaggio96@gmail.com>
2023-02-20 17:51:35 +0100

    mada project model and controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +21,8 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth', 'verified'])->prefix("admin")->name('admin.')->group(function(){
-    Route::get('/dashboard', [DashboardController::class, "index"]);
+    Route::get('/dashboard', [DashboardController::class, "index"])->name("dashboard.");
+    Route::resource("/project", ProjectController::class);
 });
 
 Route::middleware('auth')->group(function () {
